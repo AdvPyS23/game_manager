191bea6dbe598991ba6fabd485410c203b4e9981
samira_student_account <sabrina.rasch@students.unibe.ch>
2023-05-08 09:28:55 +0200

    remove "initialise_gm()" to make everything global; in class "Game" check input of user

--- gm.py
+++ gm.py
@@ -1,7 +1,6 @@
 """
 This is the main script to run the game manager.
 
-
 Classes:
     Game
         _summary_
@@ -15,59 +14,53 @@ Functions:
 """
 
 from datetime import datetime
+from itertools import chain, repeat
 import numpy as np
 import pandas as pd
 
-def initialise_gm():
-    global NUM_POINTS
-    global TOPICS
-    global SKILLS
-    global PHYSICAL_PARTS
-    global SOCIAL_TYPES
-    global GAME_DETAILS
-    global DETAIL_DF
-    # Define the number of points to give for details such as complexity and difficulty
-    NUM_POINTS =  10
-    # Define the values to choose from for details such as topics, skills etc.
-    TOPICS = ("Fantasy", "Science Fiction", "Real World", "Abstract", "Adaptation", "other")
-    SKILLS = ("Logics", "Dexterity", "Intuition", "Creativity", "Knowledge", "Strategy", "Negotiation", "Luck", "Roleplay")
-    PHYSICAL_PARTS = ("board", "cards", "dice", "supplementals", "other")
-    SOCIAL_TYPES = ("cooperative", "one_v_all", "teams", "all_v_all", "other")
-    # Define all the details a game has information about
-    GAME_DETAILS = ("min_num_players",
-                    "max_num_players",
-                    "min_duration",
-                    "max_duration",
-                    "min_age",
-                    "complexity",
-                    "difficulty",
-                    "topic",
-                    "skills",
-                    "physical_parts",
-                    "social_type")
-    detail_cols = ("string", "type", "allowed_values")
-    DETAIL_DF = pd.DataFrame(np.array([["minimum number of players", "int", ""],
-                                          ["maximum number of players", "int", ""],
-                                          ["minimum duration (minutes)", "int", ""],
-                                          ["maximum duration (minutes)", "int", ""],
-                                          ["minimum age (years)", "int", ""],
-                                          [f"complexity level (1 - {NUM_POINTS})", "int_range", "NUM_POINTS"], # tuple(n+1 for n in range(NUM_POINTS))],
-                                          [f"difficulty level (1 - {NUM_POINTS})", "int_range", "NUM_POINTS"],  # tuple(n+1 for n in range(NUM_POINTS))],
-                                          [f"topic ({', '.join(TOPICS)})", "choice", "TOPICS"], # TOPICS],
-                                          [f"skill needed ({', '.join(SKILLS)})", "choice", "SKILLS"], # SKILLS],
-                                          [f"physical part ({', '.join(PHYSICAL_PARTS)})", "choice", "PHYSICAL_PARTS"], # PHYSICAL_PARTS],
-                                          [f"social type ({', '.join(SOCIAL_TYPES)})", "choice", "SOCIAL_TPYES"]]), # SOCIAL_TYPES]]))
-                                columns = detail_cols,
+# Define the number of points to give for details such as complexity and difficulty
+NUM_POINTS =  10
+# Define the values to choose from for details such as topics, skills etc.
+TOPICS = ("Fantasy", "Science Fiction", "Real World", "Abstract", "Adaptation", "other")
+SKILLS = ("Logics", "Dexterity", "Intuition", "Creativity", "Knowledge", "Strategy", "Negotiation", "Luck", "Roleplay")
+PHYSICAL_PARTS = ("board", "cards", "dice", "supplementals", "other")
+SOCIAL_TYPES = ("cooperative", "one_vs_all", "teams", "all_vs_all", "other")
+# Define all the details a game has information about
+GAME_DETAILS = ("min_num_players",
+                "max_num_players",
+                "min_duration",
+                "max_duration",
+                "min_age",
+                "complexity",
+                "difficulty",
+                "topic",
+                "skills",
+                "physical_parts",
+                "social_type")
+DETAILS_COLS = ("string", "type", "allowed_values")
+DETAIL_DF = pd.DataFrame(np.array( [["minimum number of players", "int", ">=1"],
+                                    ["maximum number of players", "int", ">=min_num_players"],
+                                    ["minimum duration (minutes)", "int", ">=1"],
+                                    ["maximum duration (minutes)", "int", ">=min_duration"],
+                                    ["minimum age (years)", "int", ">=1"],
+                                    [f"complexity level (1 - {NUM_POINTS})", "int_range", "1 - NUM_POINTS"],
+                                    [f"difficulty level (1 - {NUM_POINTS})", "int_range", "1 - NUM_POINTS"],
+                                    [f"topic ({', '.join(TOPICS)})", "string", "TOPICS"],
+                                    [f"skill needed ({', '.join(SKILLS)})", "string", "SKILLS"],
+                                    [f"physical part ({', '.join(PHYSICAL_PARTS)})", "string", "PHYSICAL_PARTS"],
+                                    [f"social type ({', '.join(SOCIAL_TYPES)})", "string", "SOCIAL_TYPES"]]),
+                                columns = DETAILS_COLS,
                                 index = GAME_DETAILS)
-    # Consolidate and return in a dictionary
-    game_properties = {"NUM_POINTS": tuple(n+1 for n in range(NUM_POINTS)),
-                       "TOPICS": TOPICS,
-                       "SKILLS": SKILLS,
-                       "PHYSICAL_PARTS": PHYSICAL_PARTS,
-                       "SOCIAL_TYPES": SOCIAL_TYPES,
-                       "GAME_DETAILS": GAME_DETAILS,
-                       "DETAIL_DF": DETAIL_DF}
-    return game_properties
+GAME__PROPERTIES = { ">=1": [">=1"],
+                    ">=min_num_players" : [">=minimum number of players"],
+                    ">=min_duration" :[">=minimum duration"],
+                    "1 - NUM_POINTS": [f"1 - {NUM_POINTS}"],
+                    "TOPICS": TOPICS,
+                    "SKILLS": SKILLS,
+                    "PHYSICAL_PARTS": PHYSICAL_PARTS,
+                    "SOCIAL_TYPES": SOCIAL_TYPES,
+                    "GAME_DETAILS": GAME_DETAILS,
+                    "DETAIL_DF": DETAIL_DF}
 
 class Game:
     """
@@ -110,7 +103,15 @@ class Game:
 
     def ask_details(self):
         for detail in GAME_DETAILS:
-            self.details[detail] = input(f"What is the {DETAIL_DF.loc[detail, 'string']}? ")
+            prompts = chain([f"What is the {DETAIL_DF.loc[detail, 'string']}?"], repeat(f"Sorry, the input must be {', '.join(GAME__PROPERTIES[DETAIL_DF['allowed_values'][detail]])}. Try again:"))
+            replies = map(input, prompts)
+            if DETAIL_DF.loc[detail, 'type'] == "int":
+                valid_response = next(filter(lambda replies: (replies.isdigit() and int(replies)>0), replies))
+            elif DETAIL_DF.loc[detail, 'type'] == "int_range":
+                valid_response = next(filter(lambda replies: (replies.isdigit() and 1 <= int(replies) <= NUM_POINTS), replies))
+            elif DETAIL_DF.loc[detail, 'type'] == "string":
+                valid_response = next(filter(GAME__PROPERTIES[DETAIL_DF['allowed_values'][detail]].__contains__, replies))
+            self.details[detail] = valid_response
     def get_details(self):
         return self.details
     def print_details(self):
@@ -118,7 +119,12 @@ class Game:
             print(f"The {DETAIL_DF.loc[detail, 'string']} of {self.name} is {self.details[detail]}")
 
     def set_single_detail(self, detail, value):
-        self.details[detail] = value
+        if DETAIL_DF.loc[detail, 'type'] == "int" and int(value)>0:
+            self.details[detail] = value
+        elif DETAIL_DF.loc[detail, 'type'] == "int_range" and value.isdigit() and 1 <= int(value) <= NUM_POINTS:
+            self.details[detail] = value
+        elif DETAIL_DF.loc[detail, 'type'] == "string" and value in GAME__PROPERTIES[DETAIL_DF['allowed_values'][detail]]:
+            self.details[detail] = value
     def get_single_detail(self, detail):
         return self.details[detail]
     def print_single_detail(self, detail):
@@ -182,13 +188,13 @@ class History:
 # gm = initialise_gm""
 # print(gm["NUM_POINTS"])
 
-initialise_gm()
+# initialise_gm()
 # print(DETAIL_DF)
-tichu = Game("Tichu")
-print(tichu)
-tichu.ask_details()
-print(tichu.get_details())
-tichu.print_details()
-tichu.set_single_detail("topic", "SciFi")
-print(tichu.get_single_detail("topic"))
-tichu.print_single_detail("topic")
+# tichu = Game("Tichu")
+# print(tichu)
+# tichu.ask_details()
+# print(tichu.get_details())
+# tichu.print_details()
+# tichu.set_single_detail("topic", "Science Fiction")
+# print(tichu.get_single_detail("topic"))
+# tichu.print_single_detail("topic")
