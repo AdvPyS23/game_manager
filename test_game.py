54fc6a0258da35be5a16569339a50c5ab8406981
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2023-05-24 21:48:06 +0200

    add testcases

--- test_game.py
+++ test_game.py
@@ -1,16 +1,35 @@
 import unittest
+import game
 from game import sort_test_choice, find_detail_attribute
 
 class TestGame(unittest.TestCase):
     def test_sort_test_choice(self):
-        self.assertEqual(sort_test_choice("123", "0123456789"), "123")
+        # Test edge cases, i.e. that first and last values are considere
+        self.assertEqual(sort_test_choice("0123", "0123"), "0123")
+        # Test sorting
         self.assertEqual(sort_test_choice("546", "0123456789"), "456")
+        # Test empty input
+        self.assertEqual(sort_test_choice("", "0123456789"), "")
+        # Test multiple occurences
         self.assertEqual(sort_test_choice("7879", "0123456789"), "789")
+        # Test for wrong input (not all digits in allowed values)
+        self.assertEqual(sort_test_choice("125", "0123"), False)
 
     def test_find_detail_attribute(self):
-        self.assertEqual(find_detail_attribute("detail", "type"), None)
-        self.assertEqual(find_detail_attribute("topic", "attribute"), None)
+        # Test wrong detail value
+        self.assertEqual(find_detail_attribute("wrong_value", "type"), None)
+        # Test wrong attribute value
+        self.assertEqual(find_detail_attribute("topic", "wrong_value"), None)
+        # Test for aboth correct values (att = "type")
         self.assertEqual(find_detail_attribute("topic", "type"), "choice")
+        # Test for aboth correct values (att = "string")
+        self.assertEqual(find_detail_attribute("topic", "string"), "topic")
+        # Test for aboth correct values (att = "allowed_values")
+        self.assertEqual(find_detail_attribute("topic", "allowed_values"), game.TOPICS)
+        # Test for aboth correct values (att = "allowed_values", different type of detail)
+        self.assertEqual(find_detail_attribute("complexity", "allowed_values"), ["1 - 10"])
+        # Test for aboth correct values (att = "allowed_values", third type of detail)
+        self.assertEqual(find_detail_attribute("min_age", "allowed_values"), [">=1"])
 
 if __name__ == "__main__":
     unittest.main()
