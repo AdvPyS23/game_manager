d092147035157dac841093066328c03192db6a8d
roman.schwob@students.unibe.ch <roman.schwob@students.unibe.ch>
2023-05-24 21:06:17 +0200

    sync lapi

--- test_game.py
+++ test_game.py
@@ -6,7 +6,7 @@ class TestGame(unittest.TestCase):
         self.assertEqual(sort_test_choice("123", "0123456789"), "123")
         self.assertEqual(sort_test_choice("546", "0123456789"), "456")
         self.assertEqual(sort_test_choice("7879", "0123456789"), "789")
-    
+
     def test_find_detail_attribute(self):
         self.assertEqual(find_detail_attribute("detail", "type"), None)
         self.assertEqual(find_detail_attribute("topic", "attribute"), None)
